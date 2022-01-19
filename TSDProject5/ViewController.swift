41ca189aee4c98e5e22590651df7973a28d2f156
GerashchenkoDmitry <finiqw@gmail.com>
2022-01-19 14:08:25 +0700

    Initial Commit

---
+++ TSDProject5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TSDProject5
+//
+//  Created by Дмитрий Геращенко on 19.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
